208962817904747bfc1b62d2b9894babaf9454a6
ashish3410 <aashishklko@gmail.com>
2025-08-01 22:00:07 +0530

    to deploy on vercel

--- todobackend/todobackend/settings.py
+++ todobackend/todobackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kce4!cu19soos_*eo33t-ibhugdq#5y)^##zd2-a%k6b3j1l6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todo',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'example',
 ]
 
 MIDDLEWARE = [
