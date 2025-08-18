e1cccc06b76057a7b39be588824a46b749363d5e
ashish3410 <aashishklko@gmail.com>
2025-08-18 17:27:11 +0530

    changes to deploy

--- todobackend/todo/admin.py
+++ todobackend/todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Todo
+@admin.register(Todo)
 
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'completed', 'created')
+    search_fields = ('title',)
+    list_filter = ('completed', 'created')
 # Register your models here.
